fa52c399d43489e08c52221d0e7e08e619f38d73
Luis <luishenriquemedina72@gmail.com>
2024-06-21 20:04:04 -0300

    Versão 1

---
+++ Curso alura logica de programação JS/Aula 1/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,18 @@
+alert('Boas vindas ao nosso site!');
+
+let nome = "Lua";
+
+let idade = 25;
+
+let numeroDeVendas = 'sexo anal';
+
+alert('Erro! Preencha todos os campos');
+
+let name = prompt('Qual é o seu nome?');
+
+let age = prompt('Qual é sua idade?');
+if (age >= 18) {
+    alert('Pode tirar habilitação.');
+}
+
+console.log(numeroDeVendas);
