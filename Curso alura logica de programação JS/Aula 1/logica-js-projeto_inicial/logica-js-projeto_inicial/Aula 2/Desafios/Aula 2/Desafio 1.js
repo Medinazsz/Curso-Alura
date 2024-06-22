92e3e113b54e7cb6c5b58294555999e33e87149c
Luis <luishenriquemedina72@gmail.com>
2024-06-21 22:46:01 -0300

    Desafio 2

---
+++ Curso alura logica de programação JS/Aula 1/logica-js-projeto_inicial/logica-js-projeto_inicial/Aula 2/Desafios/Aula 2/Desafio 1.js
@@ -0,0 +1,8 @@
+let dia = prompt("Que dia é hoje?");
+if (dia == "sabado") {
+  alert("Bom fim de semana!");
+} else if (dia == "domingo") {
+  alert("Bom fim de semana!");
+} else {
+  alert("Boa semana!");
+}
---
+++ Curso alura logica de programação JS/Aula 1/logica-js-projeto_inicial/logica-js-projeto_inicial/Aula 2/Desafios/Desafio 2.js
@@ -0,0 +1,6 @@
+let numero = prompt("Digite um numero:");
+if (numero < 0) {
+  alert("Este número é negativo");
+} else {
+  alert("Este número é positivo");
+}
